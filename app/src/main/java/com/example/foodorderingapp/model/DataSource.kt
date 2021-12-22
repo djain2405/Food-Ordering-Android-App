79bcf6ff8d64257adedcc8fd7d0ff652f074c8e9
Divya Jain <divyajain2405@gmail.com>
2021-12-21 23:24:50 -0800

    Update DataSource.kt

--- app/src/main/java/com/example/foodorderingapp/model/DataSource.kt
+++ app/src/main/java/com/example/foodorderingapp/model/DataSource.kt
@@ -7,4 +7,16 @@ import androidx.lifecycle.MutableLiveData
 class DataSource(resources: Resources) {
     private val initialCategoryList = inventoryCategoryList(resources)
     private val categoryLiveData = MutableLiveData(initialCategoryList)
+
+    companion object {
+        private var INSTANCE: DataSource? = null
+
+        fun getDataSource(resources: Resources): DataSource {
+            return synchronized(DataSource::class) {
+                val newInstance = INSTANCE ?: DataSource(resources)
+                INSTANCE = newInstance
+                newInstance
+            }
+        }
+    }
 }
