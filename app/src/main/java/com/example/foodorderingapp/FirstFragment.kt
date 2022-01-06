505f25dfe32e7001c22ee97f666a961a5c8f3317
Divya Jain <divyajain2405@gmail.com>
2022-01-05 23:25:06 -0800

    nested home recyclerview

--- app/src/main/java/com/example/foodorderingapp/FirstFragment.kt
+++ app/src/main/java/com/example/foodorderingapp/FirstFragment.kt
@@ -12,10 +12,12 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.foodorderingapp.databinding.FragmentFirstBinding
+import com.example.foodorderingapp.databinding.ItemCategoryListBinding
 import com.example.foodorderingapp.databinding.ItemInventoryCategoryBinding
 import com.example.foodorderingapp.model.InventoryCategory
 import com.example.foodorderingapp.ui.HomeViewModel
 import com.example.foodorderingapp.ui.HomeViewModelFactory
+import java.lang.IllegalArgumentException
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -27,8 +29,8 @@ class FirstFragment : Fragment() {
         HomeViewModelFactory(requireContext())
     }
 
-    private val categoryAdapter by lazy {
-        InventoryCategoryAdapter()
+    private val homeAdapter by lazy {
+        HomeAdapter()
     }
 
     // This property is only valid between onCreateView and
@@ -48,7 +50,7 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.homeRecyclerview.apply {
-            adapter = categoryAdapter
+            adapter = homeAdapter
         }
         binding.buttonFirst.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
@@ -56,7 +58,9 @@ class FirstFragment : Fragment() {
 
         homeViewModel.categoryLiveData.observe(viewLifecycleOwner, {
             it.let {
-                categoryAdapter.submitList(it)
+                val homeList = mutableListOf<HomeAdapter.HomeItem>()
+                homeList.add(HomeAdapter.HomeItem.InventoryCategoryItem(it))
+                homeAdapter.submitList(homeList)
             }
         })
     }
@@ -65,6 +69,58 @@ class FirstFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+}
+
+class HomeAdapter : ListAdapter<HomeAdapter.HomeItem, HomeAdapter.HomeViewHolder>(HomeDiffCallback) {
+    object HomeDiffCallback: DiffUtil.ItemCallback<HomeItem>() {
+        override fun areItemsTheSame(oldItem: HomeItem, newItem: HomeItem): Boolean =
+            oldItem.id == newItem.id
+
+        override fun areContentsTheSame(oldItem: HomeItem, newItem: HomeItem): Boolean =
+            oldItem == newItem
+
+    }
+
+    sealed class HomeItem(val type: Int, val id: Int) {
+        data class InventoryCategoryItem(
+            val categoryList: List<InventoryCategory>
+        ) : HomeItem(INVENTORY_CATEGORY_LIST,INVENTORY_CATEGORY_LIST)
+    }
+
+    sealed class HomeViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        abstract fun bind(homeItem: HomeItem)
+
+        class InventoryCategoryViewHolder(private val binding: ItemCategoryListBinding) : HomeViewHolder(binding.root) {
+            private val categoryAdapter by lazy {
+                InventoryCategoryAdapter()
+            }
+            override fun bind(homeItem: HomeItem) {
+                binding.categoryRecyclerview.apply {
+                    adapter = categoryAdapter
+                }
+                categoryAdapter.submitList((homeItem as HomeItem.InventoryCategoryItem).categoryList)
+            }
+
+        }
+    }
+
+    companion object {
+        private const val INVENTORY_CATEGORY_LIST = 0
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeAdapter.HomeViewHolder {
+        return when(viewType) {
+            INVENTORY_CATEGORY_LIST -> HomeViewHolder.InventoryCategoryViewHolder(
+                ItemCategoryListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            ) else -> throw IllegalArgumentException("Unsupported viewType: $viewType")
+        }
+    }
+
+    override fun onBindViewHolder(holder: HomeAdapter.HomeViewHolder, position: Int) {
+        val item = getItem(position)
+        holder.bind(item)
+    }
+
 }
 
 class InventoryCategoryAdapter :
---
+++ app/src/test/java/com/example/foodorderingapp/HomeAdapter.kt
@@ -0,0 +1,5 @@
+package com.example.foodorderingapp
+
+class HomeAdapter {
+
+}
